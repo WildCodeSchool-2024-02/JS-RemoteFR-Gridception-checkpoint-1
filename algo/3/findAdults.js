8d35a2a1c2252b2146215a0fd0f641fddb9655c9
unknown <adj.hamzaoui@gmail.com>
2024-03-25 12:11:22 +0100

    add

--- algo/3/findAdults.js
+++ algo/3/findAdults.js
@@ -25,8 +25,47 @@ const persons = [
 
 */
 
+
+
+
+
 function findAdults(persons) {
-  // Your code here !
+  const persons = [
+    { name: 'Mary Poppins', age: 32, sex: 'female' },
+    { name: 'Tony Stark', age: 36, sex: 'male' },
+    { name: 'John Doe', age: 12, sex: 'male' },
+    { name: 'Jane Doe', age: 6, sex: 'female' },
+    { name: 'Maggie Simpson', age: 2, sex: 'female' },
+    { name: 'Leonardo', age: 92, sex: 'male' },
+    { name: 'Carla Moreau', age: 24, sex: 'female' },
+    { name: 'Mickael Jackson', age: 17, sex: 'male' },
+    { name: 'Penelope Milena', age: 27, sex: 'female' },
+    { name: 'Homer Simpson', age: 45, sex: 'male' },
+    { name: 'Leonardo Di Caprio', age: 42, sex: 'male' },
+    { name: 'Carla Bruni', age: 54, sex: 'female' },
+    { name: 'Lisa Simpson', age: 13, sex: 'female' },
+    { name: 'Millie Bobby Brown', age: 15, sex: 'female' },
+    { name: 'Penelope Cruz', age: 47, sex: 'female' },
+  ];
+  const adultFemales = [];
+  const adultMales = [];
+
+for (let i = 0; i < 15; i++) {
+  if (persons[i].sex==="female") 
+    adultFemales.push(persons[i]);
+    if(persons[i].age>=18)
+    adultFemales.push(persons[i]);
+if (persons[i].sex==="male") 
+adultMales.push(persons[i]);
+ if(persons[i].age>=18)
+adultMales.push(persons[i]);}
+
+return (adultMales); 
+
+  
+
 }
+findAdults();
+
 
 module.exports = findAdults;
