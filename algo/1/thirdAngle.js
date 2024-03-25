28eca2c339f659be022cf5ff640b87911c061037
unknown <sebastien.creuze@live.fr>
2024-03-25 09:29:00 +0100

    finish algo 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,11 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  const sumOfAngles = a + b;
+  const thirdAngle = 180 - sumOfAngles;
+
+  return thirdAngle;
 }
+thirdAngle(90, 30);
 
 module.exports = thirdAngle;
