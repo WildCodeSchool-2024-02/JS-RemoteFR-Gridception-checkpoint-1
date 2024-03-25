a7acafc6835e95fcd961a6e23bee0c1a3b7d1573
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-03-25 12:58:55 +0100

    amélioration de la position de la nav CSS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,6 +8,7 @@ imgAvatar.addEventListener('click', function () {
 const buttonModify = document.querySelector("#button_modify");
 const firstName = document.querySelector("#firstname");
 const backgroundSection = document.querySelector(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text")
 
 buttonModify.addEventListener('click', function () {
     const nameUser = prompt("What's your name ?");
@@ -15,7 +16,11 @@ buttonModify.addEventListener('click', function () {
 
     firstName.style.color = "white";
 
-    const colorUser = prompt("What color for the background ?");
-    backgroundSection.style.backgroundColor = colorUser;
+    // const colorUser = prompt("What color for the background ?");
+    // backgroundSection.style.backgroundColor = colorUser;
+
+    const colorUser2 = prompt("What color for all pink ?");
+    backgroundSection.style.backgroundColor = colorUser2;
+    pinkText.style.color = colorUser2;
 
 });
