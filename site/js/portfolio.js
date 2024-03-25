48af3cc3d50056e588e3d23c166a224eb5d1bbc5
NicoGER1 <nico3g@hotmail.fr>
2024-03-25 12:50:22 +0100

    step 2 JS done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,10 +1,20 @@
 // change image src
 
 const image = document.getElementById("avatar");
-image.addEventListener("click", function(){
-    image.src = "image/avatar-bis.png";
+image.addEventListener("click", function () {
+  image.src = "image/avatar-bis.png";
 });
 
-// prompt for name 
+// prompt for name
 
+const createButton = document.createElement("button");
+createButton.textContent = "Enter your Name";
 
+document.querySelector(".description").append(createButton);
+
+createButton.addEventListener("click", function () {
+  const name = prompt("Enter your name");
+  const firstName = document.querySelector("#firstname");
+  firstName.textContent = name;
+  firstName.style.color = "white";
+});
