68f1a1ff30191e41fe47e6dccd3a0c507721ec75
pierre delarocque <delarocque.pierre@gmail.com>
2024-03-25 13:02:56 +0100

    last push

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,5 +5,30 @@ changeImage.addEventListener("click", function () {
   changeImage.alt = "a dog";
 });
 
-const changeName = document.querySelector("#firstname");
-changeName.innerText = "Pierre";
+/*const changeName = document.querySelector("#firstname");*/
+const pushButton = document.querySelector("button");
+pushButton.addEventListener("click", () => {
+  const newName = prompt("Votre nom ?");
+  if (newName !== null) {
+    const changeName = document.querySelector("#firstname");
+    changeName.textContent = newName;
+    changeName.classList.add("");
+  }
+  const newColor = prompt("quelle couleur");
+  if (newColor !== null) {
+    const changeColor = document.querySelector(".description pink-bg");
+    changeColor.style.backgroundColor = newColor;
+    changeColor.classList.add("");
+  }
+});
+
+/*essaie step 2*/
+
+/*const pushButton = document.querySelector("button");
+pushButton.addEventListener("click", () => {
+  const newColor = prompt("Quelle couleur");
+  if (newColor !== nul) {
+    const changeColor = document.querySelector(".description pink-bg");
+    changeColor.box.style.backgroundColor = newColor;
+  }
+});*/
