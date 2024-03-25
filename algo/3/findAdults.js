b98ef5f682ed2843d371fb3694c32967bf58f249
NicoGER1 <nico3g@hotmail.fr>
2024-03-25 10:04:16 +0100

    resolving algo 3

--- algo/3/findAdults.js
+++ algo/3/findAdults.js
@@ -6,27 +6,32 @@ lui-même contenant deux sous-tableaux :
 (Étant donné qu'une personne est un adulte si elle a 18 ans ou plus)
 
 const persons = [
-  { name: 'Mary Poppins', age: 32, sex: 'female' },
-  { name: 'Tony Stark', age: 36, sex: 'male' },
-  { name: 'John Doe', age: 12, sex: 'male' },
-  { name: 'Jane Doe', age: 6, sex: 'female' },
-  { name: 'Maggie Simpson', age: 2, sex: 'female' },
-  { name: 'Leonardo', age: 92, sex: 'male' },
-  { name: 'Carla Moreau', age: 24, sex: 'female' },
-  { name: 'Mickael Jackson', age: 17, sex: 'male' },
-  { name: 'Penelope Milena', age: 27, sex: 'female' },
-  { name: 'Homer Simpson', age: 45, sex: 'male' },
-  { name: 'Leonardo Di Caprio', age: 42, sex: 'male' },
-  { name: 'Carla Bruni', age: 54, sex: 'female' },
-  { name: 'Lisa Simpson', age: 13, sex: 'female' },
-  { name: 'Millie Bobby Brown', age: 15, sex: 'female' },
-  { name: 'Penelope Cruz', age: 47, sex: 'female' },
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "John Doe", age: 12, sex: "male" },
+  { name: "Jane Doe", age: 6, sex: "female" },
+  { name: "Maggie Simpson", age: 2, sex: "female" },
+  { name: "Leonardo", age: 92, sex: "male" },
+  { name: "Carla Moreau", age: 24, sex: "female" },
+  { name: "Mickael Jackson", age: 17, sex: "male" },
+  { name: "Penelope Milena", age: 27, sex: "female" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Lisa Simpson", age: 13, sex: "female" },
+  { name: "Millie Bobby Brown", age: 15, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
 ];
-
 */
-
 function findAdults(persons) {
-  // Your code here !
+  const adultFemales = persons.filter(
+    (persons) => persons.age >= 18 && persons.sex === "female"
+  );
+  const adultMales = persons.filter(
+    (persons) => persons.age >= 18 && persons.sex === "male"
+  );
+ return [ adultFemales, adultMales];
+
 }
 
 module.exports = findAdults;
