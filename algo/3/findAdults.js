15d6208f2eff89d6ed8b2d0ababc6ba935b60691
unknown <sebastien.creuze@live.fr>
2024-03-25 09:56:48 +0100

    finish algo 3

--- algo/3/findAdults.js
+++ algo/3/findAdults.js
@@ -27,6 +27,41 @@ const persons = [
 
 function findAdults(persons) {
   // Your code here !
+  const femmesAdultes = [];
+  const hommesAdultes = [];
+
+  persons.forEach((person) => {
+    if (person.age >= 18) {
+      if (person.sex === "female") {
+        femmesAdultes.push(person);
+      } else if (person.sex === "male") {
+        hommesAdultes.push(person);
+      }
+    }
+  });
+
+  return [femmesAdultes, hommesAdultes];
 }
+
+const persons = [
+  { name: "Mary Poppins", age: 32, sex: "female" },
+  { name: "Tony Stark", age: 36, sex: "male" },
+  { name: "John Doe", age: 12, sex: "male" },
+  { name: "Jane Doe", age: 6, sex: "female" },
+  { name: "Maggie Simpson", age: 2, sex: "female" },
+  { name: "Leonardo", age: 92, sex: "male" },
+  { name: "Carla Moreau", age: 24, sex: "female" },
+  { name: "Mickael Jackson", age: 17, sex: "male" },
+  { name: "Penelope Milena", age: 27, sex: "female" },
+  { name: "Homer Simpson", age: 45, sex: "male" },
+  { name: "Leonardo Di Caprio", age: 42, sex: "male" },
+  { name: "Carla Bruni", age: 54, sex: "female" },
+  { name: "Lisa Simpson", age: 13, sex: "female" },
+  { name: "Millie Bobby Brown", age: 15, sex: "female" },
+  { name: "Penelope Cruz", age: 47, sex: "female" },
+];
+
+const result = findAdults(persons);
+console.log(result);
 
 module.exports = findAdults;
